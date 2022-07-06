d2c7042be1843801ae8c7010f0af23e290fc9066
yellowwenjie <yellowwenjie@gmail.com>
2022-07-06 22:52:24 +0800

    vue3

--- src/pages/Dashboard/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react'
-
-function Dashboard() {
-  return <>Dashboard！</>
-}
-
-export default Dashboard
--- src/utils/token.ts
+++
@@ -1,8 +0,0 @@
-export const setToken = (userToken: string):void => {
-  sessionStorage.setItem('token',JSON.stringify(userToken));
-}
-
-export const getToken = ():string | null => {
-  return sessionStorage.getItem('token');
-}
-
